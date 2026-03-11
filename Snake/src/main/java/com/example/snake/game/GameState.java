f26da7ffe1603283aed14d32afe4a6404a5cb8f4
im24a-ademaja <ademaja@bzz.ch>
2026-03-11 09:56:41 +0100

    Warnungen loswerden

--- Snake/src/main/java/com/example/snake/game/GameState.java
+++ Snake/src/main/java/com/example/snake/game/GameState.java
@@ -1,13 +1,11 @@
 package com.example.snake.game;
-
 import lombok.Getter;
 import lombok.Setter;
+
 import java.util.List;
 
 @Getter
 @Setter
-
-
 public class GameState {
 
     private List<Position> snake;
@@ -19,36 +17,4 @@ public class GameState {
     private int score;
 
     private boolean gameOver;
-
-    public List<Position> getSnake() {
-        return snake;
-    }
-
-    public void setSnake(List<Position> snake) {
-        this.snake = snake;
-    }
-
-    public Position getApple() {
-        return apple;
-    }
-
-    public void setApple(Position apple) {
-        this.apple = apple;
-    }
-
-    public Direction getDirection() {
-        return direction;
-    }
-
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
 }
