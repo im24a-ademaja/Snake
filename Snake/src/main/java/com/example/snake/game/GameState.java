f42942f5a51a89f9fe001d3db808c415ab4d5c54
im24a-ademaja <ademaja@bzz.ch>
2026-03-11 09:35:23 +0100

    Ausarbeitung des Skripts für GameState, Position

--- Snake/src/main/java/com/example/snake/game/GameState.java
+++ Snake/src/main/java/com/example/snake/game/GameState.java
@@ -1,12 +1,54 @@
 package com.example.snake.game;
 
+import lombok.Getter;
+import lombok.Setter;
 import java.util.List;
-import java.awt.Point;
+
+@Getter
+@Setter
+
 
 public class GameState {
-    private List<Point> snake;
-    private Point apple;
+
+    private List<Position> snake;
+
+    private Position apple;
+
     private Direction direction;
+
     private int score;
+
     private boolean gameOver;
+
+    public List<Position> getSnake() {
+        return snake;
+    }
+
+    public void setSnake(List<Position> snake) {
+        this.snake = snake;
+    }
+
+    public Position getApple() {
+        return apple;
+    }
+
+    public void setApple(Position apple) {
+        this.apple = apple;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
