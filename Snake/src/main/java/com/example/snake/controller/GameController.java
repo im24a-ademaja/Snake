08f2f48c8e64b203b00c12c7d0571ac6b7d2352e
im24a-ademaja <ademaja@bzz.ch>
2026-03-11 09:51:21 +0100

    Konfiguration GameController

--- Snake/src/main/java/com/example/snake/controller/GameController.java
+++ Snake/src/main/java/com/example/snake/controller/GameController.java
@@ -1,9 +1,33 @@
 package com.example.snake.controller;
 
+import com.example.snake.game.GameService;
+import com.example.snake.game.GameState;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/game")
 public class GameController {
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @PostMapping("/start")
+    public void startGame() {
+        gameService.startGame();
+    }
+
+    @PostMapping("/move")
+    public void move() {
+        gameService.move();
+    }
+
+    @GetMapping("/state")
+    public GameState getState() {
+        return gameService.getGameState();
+    }
 }
